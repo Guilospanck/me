d4a08903d12a73427f80bced6cd31e1d68a39507
Guilherme Pereira <guilospanck@protonmail.com>
2023-04-28 08:52:43 +0100

    Fix env variable routing when using ghpages

--- src/Route.tsx
+++ src/Route.tsx
@@ -3,15 +3,9 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 const Home = React.lazy(() => import('./pages/Home'))
 
-export const AppRoute = () => {
-  // const BASENAME = process.env.NODE_ENV === 'development' ? '/' : '/me' // because of github pages
-  const BASENAME = '/'
-
-  return (
-    <BrowserRouter basename={BASENAME}>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </BrowserRouter>
-  )
-}
+export const AppRoute = () =>
+  <BrowserRouter basename={process.env.BASENAME}>
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  </BrowserRouter>
--- webpack.dev.js
+++ webpack.dev.js
@@ -9,31 +9,35 @@ const { merge } = require('webpack-merge')
 
 const fs = require('fs')
 
-module.exports = merge(common, {
-  mode: 'development',
-  devtool: 'inline-source-map',
-  devServer: {
-    static: {
-      directory: path.join(__dirname, 'build'),
-      watch: true
-    },    
-    historyApiFallback: true,
-    port: 3000,
-    server: {
-      type: 'https',
-      options: {
-        key: fs.readFileSync(path.join(__dirname, 'certs/localhost-key.pem')),
-        cert: fs.readFileSync(path.join(__dirname, 'certs/localhost.pem')),
+module.exports = (env) => {
+  const envfile = env.envfile || 'development'
+
+  return merge(common, {
+    mode: 'development', // automatically sets NODE_ENV to development
+    devtool: 'inline-source-map',
+    devServer: {
+      static: {
+        directory: path.join(__dirname, 'build'),
+        watch: true
+      },
+      historyApiFallback: true,
+      port: 3000,
+      server: {
+        type: 'https',
+        options: {
+          key: fs.readFileSync(path.join(__dirname, 'certs/localhost-key.pem')),
+          cert: fs.readFileSync(path.join(__dirname, 'certs/localhost.pem'))
+        }
       }
     },
-  },
-  plugins: [
-    new Dotenv({
-      path: '.env.development'
-    }),
-    new HtmlWebpackPlugin({
-      template: 'template.dev.html',
-    }),
-    new FaviconsWebpackPlugin('src/assets/images/logo-512x512.png')
-  ]
-})
+    plugins: [
+      new Dotenv({
+        path: `.env.${envfile}`
+      }),
+      new HtmlWebpackPlugin({
+        template: 'template.dev.html'
+      }),
+      new FaviconsWebpackPlugin('src/assets/images/logo-512x512.png')
+    ]
+  })
+}
